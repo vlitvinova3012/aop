d6ea9f9ff963fe499cb1a841c983adf8c112290b
Litvinova Viktoria <vlitvinova@diasoft.ru>
2023-09-12 17:32:24 +0300

    Initial commit

---
+++ src/main/java/com/spring/aop/Greeting.java
@@ -0,0 +1,12 @@
+package com.spring.aop;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Greeting {
+    private final long id;
+    private final String content;
+
+}
---
+++ src/main/java/com/spring/aop/controllers/GreetingController.java
@@ -0,0 +1,20 @@
+package com.spring.aop.controllers;
+
+import com.spring.aop.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+    private final AtomicLong atomicLong = new AtomicLong();
+    private static final String temp = "Hello %s";
+    @GetMapping
+    public Greeting greeting(@RequestParam (value = "name", defaultValue = "World") String name) {
+       return new Greeting(atomicLong.incrementAndGet(), String.format(temp,name));
+    }
+}
